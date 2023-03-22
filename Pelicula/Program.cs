dd28d2c2a283128849f9df9d498d57d852965127
Jennifer Macedo <126447294+JenniferMC62@users.noreply.github.com>
2023-03-22 03:33:46 +0000

    update

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -113,11 +113,7 @@ namespace Pelicula
         // ImprimeActores()
         public void ImprimeActores()
         {
-            Console.WriteLine("Actores de la película '{0}':", titulo);
-            foreach (Actor actor in actores)
-            {
-                Console.WriteLine("-{0} ({1})", actor.Nombre, actor.Año);
-            }
+            Console.WriteLine($"{Nombre} ({Año})");
         }
 
     }
@@ -216,7 +212,6 @@ namespace Pelicula
             {
                 Console.WriteLine("{0} ({1})", p.GetTitulo(), p.GetAño());
             }
-            //
         }
 
     }
